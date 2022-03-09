661800380ff7c4a1cbadfe718105596384e8572e
yubinquitous <yb9854@naver.com>
2022-03-09 14:40:16 +0900

    [solong] rename minilibx

--- so_long/src/main.c
+++ so_long/src/main.c
@@ -1,18 +1,70 @@
 #include <stdio.h>
-#include "mlx.h"
+#include <stdlib.h>
+#include "../mlx/mlx.h"
 
-int	main(void)
+#define X_EVENT_KEY_PRESS		2
+#define X_EVENT_KEY_release		3
+#define X_EVENT_KEY_EXIT		17 //exit key code
+
+//Mac key code example
+//All the key code example other than below is described on the site linked in READEME.md
+#define KEY_ESC			53
+# define KEY_Q			12
+# define KEY_W			13
+# define KEY_E			14
+# define KEY_R			15
+# define KEY_A			0
+# define KEY_S			1
+# define KEY_D			2
+
+//Since key_press() can recieve only one argument, all the argument shold be gathered in one structure
+//x,y and str are meaningless variables.
+typedef struct s_param{
+	int		x;
+	int		y;
+	char	str[3];
+}				t_param;
+
+//Only param->x will be used. 
+void			param_init(t_param *param)
 {
-	void	*window;
-	void	*mlx;
-	void	*img;
-	int		width;
-	int		height;
+	param->x = 3;
+	param->y = 4;
+	param->str[0] = 'a';
+	param->str[1] = 'b';
+	param->str[2] = '\0';
+}
 
-	mlx = mlx_init();
-	window = mlx_new_window(mlx, 1000, 1000, "yubin");
-	img = mlx_xpm_file_to_image(mlx, "./kirby.xpm", &width, &height);
-	mlx_put_image_to_window(mlx, window, img, 0, 0);
-	mlx_loop(mlx);
+int				key_press(int keycode, t_param *param)
+{
+	static int a = 0;
+
+	if (keycode == KEY_W)//Action when W key pressed
+		param->x++;
+	else if (keycode == KEY_S) //Action when S key pressed
+		param->x--;
+	else if (keycode == KEY_ESC) //Quit the program when ESC key pressed
+		exit(0);
+	printf("x: %d\n", param->x);
 	return (0);
+}
+
+int			main(void)
+{
+	void		*mlx;
+	void		*win;
+	t_param		param;
+
+	param_init(&param);
+	mlx = mlx_init();
+	win = mlx_new_window(mlx, 800, 600, "yubchoi");
+	printf("-------------------------------\n");
+	printf("'W key': Add 1 to x.\n");
+	printf("'S key': Subtract 1 from x\n");
+	printf("'ESC key': Exit this program\n");
+	printf("'Other keys': print current x \n");
+	printf("-------------------------------\n");
+	printf("Current x = 3\n");
+	mlx_hook(win, X_EVENT_KEY_PRESS, 0, &key_press, &param);
+	mlx_loop(mlx);
 }
