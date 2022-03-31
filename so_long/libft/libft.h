f9594932f7f289175dad200c425335ba69a56b5c
yubinquitous <yb9854@naver.com>
2022-03-31 17:27:42 +0900

    [so_long] norm check

--- so_long/libft/libft.h
+++ so_long/libft/libft.h
@@ -6,19 +6,18 @@
 /*   By: yubchoi <yubchoi@student.42>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/06 16:37:32 by yubchoi           #+#    #+#             */
-/*   Updated: 2022/03/25 16:21:26 by yubchoi          ###   ########.fr       */
+/*   Updated: 2022/03/31 17:26:52 by yubchoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef LIBFT_H
+# define LIBFT_H
 
 # include <unistd.h>
 # include <stdlib.h>
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE	4096
-# endif
 
 char	*get_next_line(int fd);
 char	*get_line(char *save);
@@ -29,8 +28,9 @@ char	*ft_strchr(const char *s, int c);
 char	*ft_strdup(const char *s1);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
-void    ft_putchar_fd(char c, int fd);
+void	ft_putchar_fd(char c, int fd);
 char	**ft_split(char const *s, char c);
 char	*ft_strjoin(char const *s1, char const *s2);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
-#endif
+
+# endif
