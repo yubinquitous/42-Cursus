68bb4d4c66f4d6b4cfb711e27665c669c4129e88
yubinquitous <yb9854@naver.com>
2022-06-14 20:05:02 +0900

    refactoring

--- push_swap/src/count/count_sort.c
+++ push_swap/src/count/count_sort.c
@@ -7,6 +7,30 @@ int select_bigger(int a, int b)
     return (b);
 }
 
+void compare_up_down(int uu, int ud, int du, int dd, t_sort *temp)
+{
+    if (uu < ud && uu < du && uu < dd)
+    {
+        temp->command_min = uu;
+        temp->flag = UU;
+    }
+    else if (ud < uu && ud < du && ud < dd)
+    {
+        temp->command_min = ud;
+        temp->flag = UD;
+    }
+    else if (du < uu && du < ud && du < dd)
+    {
+        temp->command_min = du;
+        temp->flag = DU;
+    }
+    else
+    {
+        temp->command_min = dd;
+        temp->flag = DD;
+    }
+}
+
 int count_up(t_stack s, int idx)
 {
     int length;
@@ -41,25 +65,5 @@ void count_command(t_dual_stack *ds, t_sort *temp)
     ud = count_up(ds->a, temp->a_idx) + count_down(ds->b, temp->b_idx);
     du = count_down(ds->a, temp->a_idx) + count_up(ds->b, temp->b_idx);
     dd = select_bigger(count_down(ds->a, temp->a_idx), count_down(ds->b, temp->b_idx));
-
-    if (uu < ud && uu < du && uu < dd)
-    {
-        temp->command_min = uu;
-        temp->flag = UU;
-    }
-    else if (ud < uu && ud < du && ud < dd)
-    {
-        temp->command_min = ud;
-        temp->flag = UD;
-    }
-    else if (du < uu && du < ud && du < dd)
-    {
-        temp->command_min = du;
-        temp->flag = DU;
-    }
-    else
-    {
-        temp->command_min = dd;
-        temp->flag = DD;
-    }
+    compare_up_down(uu, ud, du, dd, temp);
 }
