07931d582da847543599ef0a6f7b53084fa56a05
yubinquitous <yb9854@naver.com>
2022-06-15 19:57:38 +0900

    [FEAT] checker 구현

--- push_swap/src/checker/checker.c
+++ push_swap/src/checker/checker.c
@@ -6,27 +6,75 @@
 /*   By: yubin <yubchoi@student.42>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/15 16:27:10 by yubin             #+#    #+#             */
-/*   Updated: 2022/06/15 17:00:48 by yubin            ###   ########.fr       */
+/*   Updated: 2022/06/15 19:57:16 by yubin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/push_swap.h"
 #include "../../libgnl/get_next_line.h"
-#include <stdio.h>
+#include <stdlib.h>
 
-void  do_command(t_dual_stack *ds)
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t			i;
+
+	i = 0;
+	while (s1[i] && s2[i] && i < n)
+	{
+		if (s1[i] != s2[i])
+			return (0);
+		++i;
+	}
+	if (i < n || s1[i])
+		return (0);
+	return (1);
+}
+
+static void do_command(t_dual_stack *ds, char *command)
+{
+    if (ft_strncmp(command, "pa\n", 3))
+        pa(ds, 0);
+    else if (ft_strncmp(command, "pb\n", 3))
+        pb(ds, 0);
+    else if (ft_strncmp(command, "ra\n", 3))
+        ra(&ds->a, 0);
+    else if (ft_strncmp(command, "rb\n", 3))
+        rb(&ds->b, 0);
+    else if (ft_strncmp(command, "rr\n", 3))
+        rr(ds, 0);
+    else if (ft_strncmp(command, "rra\n", 4))
+        rra(&ds->a, 0);
+    else if (ft_strncmp(command, "rrb\n", 4))
+        rrb(&ds->b, 0);
+    else if (ft_strncmp(command, "rrr\n", 4))
+        rrr(ds, 0);
+    else if (ft_strncmp(command, "sa\n", 3))
+        sa(&ds->a, 0);
+    else if (ft_strncmp(command, "sb\n", 3))
+        sb(&ds->b, 0);
+    else if (ft_strncmp(command, "ss\n", 3))
+        ss(ds, 0);
+    else 
+        error_exit();
+}
+
+void  do_input(t_dual_stack *ds)
 {
     char    *command;
 
     while (1)
     {
         command = get_next_line(0);
-        while (!command)
+        if (!command || *command == '\n')
             break;
-        printf("command : %s\n", command);
+        do_command(ds, command);
         free(command);
+        command = 0;
     }
-    
+    if (stack_is_empty(ds->b) && stack_is_sorted(ds->a))
+        write(1, "OK\n", 3);
+    else
+        write(1, "KO\n", 3);
 }  
 
 int main(int argc, char** argv)
@@ -40,5 +88,6 @@ int main(int argc, char** argv)
     init_stack(argc, argv, cnt, &ds);
     if (stack_is_sorted(ds.a))
         return (0);
-    do_command(&ds);
+    do_input(&ds);
+    return (0);
 }
