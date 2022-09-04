76bdc8866d85a613a3d5ce70f6352762133183d4
yubinquitous <yb9854@naver.com>
2022-09-04 16:57:37 +0900

    [CHORE] header 정리 (#4)

--- 03-philosophers/philo/philosophers.h
+++ 03-philosophers/philo/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: yubchoi <yubchoi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/08 18:44:04 by yubin             #+#    #+#             */
-/*   Updated: 2022/09/04 15:59:22 by yubchoi          ###   ########.fr       */
+/*   Updated: 2022/09/04 16:57:19 by yubchoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,9 +77,6 @@ typedef struct s_observer
 } t_observer;
 
 void acquire_forks(t_philo *philo);
-char anyone_starving_everyone_full(t_info info, t_philo *philo);
-void broadcast_end(t_end_state *end_state);
-void destroy_mutex(int n_philo, t_philo *philo, t_end_state *end_state);
 void destroy_mutex_fork_event(int i, t_philo *philo, t_end_state *end_state);
 int ft_atoi(char *str);
 char ft_malloc_philo(t_philo **philo, int n_philo);
@@ -89,20 +86,16 @@ int init_mutex(t_info *info, t_philo *philo, t_end_state *end_state);
 int init_mutex_fork_event(pthread_mutex_t *fork, pthread_mutex_t *event);
 void init_observer(t_observer *observer, t_info info, t_philo *philo, t_end_state *end_state);
 void init_philo(t_info info, t_philo *philo, t_end_state *end_state);
-void join_all(int n_philo, t_philo *philo);
 void logger(t_philo *philo, enum e_philo_status status_num);
-int main(int argc, char **argv);
 void nano_usleep(int millisec);
 void *observer_thread(t_observer *observer);
 char philo_eat(t_philo *philo);
 void *philo_one_thread(t_philo *philo);
 char philo_sleep(t_philo *philo);
-char philo_starving(t_philo *philo, int ttd);
 char philo_think(t_philo *philo);
 void *philo_thread(void *_philo);
 int print_err(enum e_status state);
 void release_forks(t_philo *philo);
-int run_observer(t_observer *observer);
 int run_simulation(t_info info, t_philo *philo, t_end_state *end_state);
 char simulation_end(t_end_state *end_state);
 void update_philo_info(t_philo *philo);
