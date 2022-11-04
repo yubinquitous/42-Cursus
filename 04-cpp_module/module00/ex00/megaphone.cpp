2cdf3456df084ee199678cfd63da3485c08b5de0
yubinquitous <yb9854@naver.com>
2022-11-04 19:16:50 +0900

    cpp init #6

---
+++ 04-cpp_module/module00/ex00/megaphone.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yubin <yubchoi@student.42>                 +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/04 19:05:06 by yubin             #+#    #+#             */
+/*   Updated: 2022/11/04 19:11:25 by yubin            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int ac, char *av[])
+{
+	if (ac == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	}
+
+	for (int i = 1; i < ac; i++)
+	{
+		for (int j = 0; av[i][j]; j++)
+		{
+			std::cout << static_cast<char>(std::toupper(av[i][j]));
+		}
+	}
+	std::cout << std::endl;
+	return (0);
+}
