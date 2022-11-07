cda4d87e0e0f82c74bb6d13fcd24d77766c30246
yubinquitous <yb9854@naver.com>
2022-11-07 20:17:13 +0900

    [CHORE] module01-ex00 main에 생성자 announce 추가

--- 04-cpp_module/module01/ex00/main.cpp
+++ 04-cpp_module/module01/ex00/main.cpp
@@ -1,13 +1,20 @@
 #include "Zombie.hpp"
 
 int main(void) {
-    Zombie zombie("Foo");
-    zombie.announce();
+    // 생성자
+    Zombie yubchoi_zombie;
+    yubchoi_zombie.announce();
 
+    //  생성자 오버로딩
+    Zombie foo_zombie("Foo");
+    foo_zombie.announce();
+
+    //  동적할당
     Zombie *new_zombie = newZombie("Goo");
     new_zombie->announce();
     delete new_zombie;
 
+    // 정적할당
     randomChump("Hoo");
 
     return 0;
