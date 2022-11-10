2f07bc7e1d7adbdd3a9d439f35ecb425ef2df69b
yubinquitous <yb9854@naver.com>
2022-11-10 10:53:55 +0900

    [FIX] module01-ex03 weapon 예외처리

--- 04-cpp_module/module01/ex03/HumanB.cpp
+++ 04-cpp_module/module01/ex03/HumanB.cpp
@@ -5,8 +5,11 @@ HumanB::HumanB(std::string name) : _name(name), _weapon() {}
 void HumanB::setWeapon(Weapon& weapon) { _weapon = &weapon; }
 
 void HumanB::attack() {
-    std::cout << _name << " attacks with their " << _weapon->getType()
-              << std::endl;
+    if (!_weapon)
+        std::cout << _name << " has no weapon" << std::endl;
+    else
+        std::cout << _name << " attacks with their " << _weapon->getType()
+                  << std::endl;
 }
 
 HumanB::~HumanB() {}
