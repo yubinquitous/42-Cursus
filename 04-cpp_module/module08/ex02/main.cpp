3c8d9542c8eb3fe8091cb597df3477244a4159de
yubinquitous <yb9854@naver.com>
2022-11-26 18:35:47 +0900

    [CHORE] main 정리

--- 04-cpp_module/module08/ex02/main.cpp
+++ 04-cpp_module/module08/ex02/main.cpp
@@ -32,11 +32,16 @@ int main() {
         mstack.push(0);
         lstack.push_back(0);
 
+        MutantStack<int> mstack2(mstack);
+
         MutantStack<int>::iterator it = mstack.begin();
         MutantStack<int>::iterator ite = mstack.end();
         std::list<int>::iterator lit = lstack.begin();
         std::list<int>::iterator lite = lstack.end();
+        MutantStack<int>::iterator it2 = mstack2.begin();
+        MutantStack<int>::iterator ite2 = mstack2.end();
 
+        // mstack : 0 737 5 3 5
         ++it;
         --it;
         std::cout << "mstack: ";
@@ -49,6 +54,12 @@ int main() {
         while (lit != lite) {
             std::cout << *lit << " ";  // 5 3 5 737 0
             ++lit;
+        }
+        std::cout << std::endl;
+        std::cout << "mstack2: ";
+        while (it2 != ite2) {
+            std::cout << *it2 << " ";  // 5 3 5 737 0
+            ++it2;
         }
         std::cout << std::endl;
     }
@@ -75,43 +86,14 @@ int main() {
         MutantStack<int>::reverse_iterator it = mstack.rbegin();
         MutantStack<int>::reverse_iterator ite = mstack.rend();
 
+        // mstack: 0 737 5 3 5
         ++it;
         --it;
         while (it != ite) {
-            std::cout << *it << std::endl;  // 0 737 5 3 5
+            std::cout << *it << " ";  // 0 737 5 3 5
             ++it;
         }
-    }
-
-    std::cout << "=========== TEST2 ===========" << std::endl;
-    {
-        MutantStack<int> mstack;
-
-        mstack.push(5);
-        mstack.push(17);
-
-        std::cout << mstack.top() << std::endl;  // 17
-
-        mstack.pop();
-
-        std::cout << mstack.size() << std::endl;  // 1
-
-        mstack.push(3);
-        mstack.push(5);
-        mstack.push(737);
-        mstack.push(0);
-
-        MutantStack<int> mstack2(mstack);
-
-        MutantStack<int>::iterator it = mstack2.begin();
-        MutantStack<int>::iterator ite = mstack2.end();
-
-        ++it;
-        --it;
-        while (it != ite) {
-            std::cout << *it << std::endl;  // 5 3 5 737 0
-            ++it;
-        }
+        std::cout << std::endl;
     }
     return 0;
 }
