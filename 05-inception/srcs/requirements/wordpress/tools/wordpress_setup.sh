04aa858f1d22559dd2817f221dd5a6c410ceb018
yubinquitous <yb9854@naver.com>
2023-03-16 15:02:36 +0900

    [FEAT] WordPress DB에 사용자 설정

---
+++ 05-inception/srcs/requirements/wordpress/tools/wordpress_setup.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+chown -R www-data:www-data /var/www/
+
+if [ ! -f "/var/www/html/wordpress/index.php" ]; then
+	sudo -u www-data sh -c " \
+	wp core download --locale=$WORDPRESS_LANG && \
+	wp config create --dbname=$WORDPRESS_DB_HOST --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbhost=$WORDPRESS_DB_HOST --dbcharset="utf8"
+	wp core install --url=$DOMAIN_NAME --title=$WORDPRESS_TITLE --admin_user=$WORDPRESS_DB_ADMIN --admin_password=$WORDPRESS_DB_ADMIN_PASSWORD --admin_email=$WORDPRESS_DB_ADMIN_EMAIL --skip-email && \
+	wp user create $WORDPRESS_USER $WORDPRESS_EMAIL --role=author --user_pass=$WORDPRESS_PASSWORD && \
+	wp plugin update --all
+	"
+fi
+
+exec /usr/sbin/php-fpm7.3 -F
